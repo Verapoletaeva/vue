ea67cdbd0de3df575dd04a4e4af84cb1060007d7
Vera <poletaeva.vera835@yandex.ru>
2018-12-06 17:31:03 +0300

    Create the project

---
+++ vue/client/src/components/SimpleButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="btn">{{ title }}</button>
+</template>
+
+<script>
+  export default {
+    name: "SimpleButton"
+  }
+
+  Vue.component('simple_button', {
+    template: '#btn-template',
+    props: {
+      title: String,
+      btnClass: String
+    }
+  })
+
+  new Vue({el: '#app'})
+</script>
+
+<style scoped>
+  .btn {
+    width: 150px;
+    height: 40px;
+    border-radius: 10px;
+    font-size: 20px;
+    color: white;
+    border: 1px solid #64bab7;
+    box-shadow: 4px 4px 7px #8bcbc9;
+  }
+
+  .blueBtn {
+    background: linear-gradient(to top, #087d78 0%,  #64BAB7 100%);
+  }
+
+  .redBtn {
+    background: linear-gradient(to top, #A51F29 0%,  #FF616C 100%);
+  }
+</style>
---
+++ vue/client/src/components/SimpleButtons.vue
@@ -0,0 +1,22 @@
+<template id="btn-template">
+  <div class="button">{{ title }}</div>
+</template>
+
+<script>
+
+  Vue.component('simple_button', {
+    props: ['title'],
+    template: '<button class="btn">{{ title }}</button>'
+  })
+
+  new Vue({el: '#button'})
+</script>
+
+<style>
+  button {
+    width: 150px;
+    height: 40px;
+    font-size: 14px;
+    border-radius: 10px;
+  }
+</style>
